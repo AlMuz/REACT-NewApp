a598b47b094890bb2312ce5a1f404b3ced89f8d5
Aleksejs Muzičenko <aleksejs@mendo.lv>
2018-10-17 13:34:12 +0300

    redux-promise, axios added and tested

---
+++ src/actions/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const API_KEY = '37db580a95548d0e019d8bc8bdf52c99';
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+
+export const FETCH_WEATHER = 'FETCH_WEATHER'
+
+export function fetchWeather(city) {
+
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
